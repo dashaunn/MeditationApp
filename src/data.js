21bd6e69bc6ab4d1464dac27d08fd2aad98221a4
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2024-01-20 21:37:51 -0600

    add data file

---
+++ src/data.js
@@ -0,0 +1,145 @@
+export default [
+    {
+        id: 1,
+        description: "",
+        backgroundImage: "northern-lights.jpeg",
+        info: {
+            title:"Falling Back to Sleep With Ease",
+            subtitle:"Dr. Eric López, Ph.D."
+        },
+        duration: 17,
+        premium: 1,
+    },
+    {
+        id: 2,
+        description: "",
+        backgroundImage: "moonlit-forest.jpeg",
+        info: {
+            title:"Journey to Rest on the Mountaintop",
+            subtitle:"Meditation • Dr. Eric López, Ph.D." 
+        },
+        duration: 29,
+        premium: 0,
+    },
+    {
+        id: 3,
+        description: "",
+        backgroundImage: "forest.png",
+        info: {
+            title:"A Woodland Stroll to Sleep",
+            subtitle:"Meditation • Prof. Megan Reitz" 
+        },
+        duration: 29,
+        premium: 1,
+    },
+    {
+        id: 4,
+        description: "",
+        backgroundImage: "beach.png",
+        info: {
+            title:"Journey Into Sleep",
+            subtitle:"Meditation • Chibs Okereke" 
+        },
+        duration: 29,
+        premium: 1,
+    },
+    {
+        id: 5,
+        description: "",
+        backgroundImage: "nightstand.jpeg",
+        info: {
+            title:"Quieting the Mind and Body",
+            subtitle:"Meditation • Prof. Megan Reitz" 
+        },
+        duration: 29,
+        premium: 1,
+    },
+    {
+        id: 6,
+        description: "",
+        backgroundImage: "bedsheets.jpeg",
+        info: {
+            title:"Soothing the Body for Rest",
+            subtitle:"Meditation • Dr. Eric López, Ph.D." 
+        },
+        duration: 45,
+        premium: 0,
+    },
+    {
+        id: 7,
+        description: "",
+        backgroundImage: "forest-animals.jpeg",
+        info: {
+            title:"Mindful Movement with Sienna & Friends",
+            subtitle:"Meditation • Will DeRenzy Martin" 
+        },
+        duration: 45,
+        premium: 1,
+    },
+    {
+        id: 8,
+        description: "",
+        backgroundImage: "sienna-sloth.jpeg",
+        info: {
+            title:"Sienna Goes Slow",
+            subtitle:"Meditation • Will DeRenzy Martin" 
+        },
+        duration: 45,
+        premium: 1,
+    },
+    {
+        id: 9,
+        description: "",
+        backgroundImage: "bed-window.jpeg",
+        info: {
+            title:"Nighttime Gratitude",
+            subtitle:"Meditation • Kirat Randhawa" 
+        },
+        duration: 45,
+        premium: 1,
+    },
+    {
+        id: 10,
+        description: "",
+        backgroundImage: "moonlit-road.jpeg",
+        info: {
+            title:"Deep Sleep",
+            subtitle:"Meditation • Tamara Levitt" 
+        },
+        duration: 30,
+        premium: 1,
+    },
+    {
+        id: 11,
+        description: "",
+        backgroundImage: "monochromatic-bedroom.jpeg",
+        info: {
+            title:"Deep Rest",
+            subtitle:"Meditation • Oren Jay Sofer" 
+        },
+        duration: 45,
+        premium: 1,
+    },
+    {
+        id: 12,
+        description: "",
+        backgroundImage: "traintracks.jpeg",
+        info: {
+            title:"Rhythm of the Train",
+            subtitle:"Series • Oren Jay Sofer" 
+        },
+        duration: 45,
+        premium: 1,
+    },
+    {
+        id: 13,
+        description: "",
+        backgroundImage: "ocean.jpeg",
+        info: {
+            title:"Sleep Rhythm: Rushing Waves",
+            subtitle:"Meditation • Tamara Levitt" 
+        },
+        duration: 36,
+        premium: 0,
+    },
+]
