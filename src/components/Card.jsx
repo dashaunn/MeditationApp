14a3f3bac911b6152525a13791d2524b95b3f6d5
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2024-01-25 20:59:06 -0600

    update Card component to return badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export default function Card(props) {
-    let badgeText
-
+    let duration
+    if(props.item.premium === 0) {
+        duration = props.item.duration;
+    }
     return (
                 <a href="" className="card">
+                    {duration && <div className="duration-badge">{duration} min</div>}
+                    {!duration && <div className="lock-badge"><img src="/src/images/lock-icon.png" className="lock-icon" /></div>}
                     <img src={`/images/${props.item.backgroundImage}`} alt="" className="card--image" />
-                    <svg src="" className="card--lockIcon"></svg>
 
                     <div className="card--info">
                         <h3 className="card--title">{props.item.info.title}</h3>
